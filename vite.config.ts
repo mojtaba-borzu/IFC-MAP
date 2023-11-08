da3b748cb10b7aa930a00a276cb5e8409d322cde
m.borzu <mojtaba.borzu69@gmail.com>
2023-11-08 20:22:42 +0330

    port changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/ifcjs-react/",
-  root: "./src",
-  publicDir: "../public",
-  server: {
-    open: true,
-  },
-  plugins: [react()],
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  define: {
-    "process.env.API_HOST": JSON.stringify(process.env.API_HOST),
-  },
+export default defineConfig(() => {
+  return {
+    define: {
+      "process.env": {},
+      "import.meta.env": {},
+    },
+    build: {
+      outDir: "build",
+    },
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      port: 3000,
+      open: true, // this will open directly to your browser
+    },
+    preview: {
+      port: 3000,
+      open: true, // this will open directly to your browser
+    },
+  };
 });
